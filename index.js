9677b40de37b946494c4ea4aad472b4e35226e3f
FayssalG <fayssaldimawac123@gmail.com>
2023-09-04 17:27:26 +0100

    Revert to initial configuration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const {Server} = require('socket.io')
-const https = require('https')
+const http = require('http')
 
 //const {getDatabase , ref , set  , get , child} = require('firebase/database')
 
@@ -10,11 +10,11 @@ const db = require('./config/firebase')
 
 
 const app = express()
-const server = https.createServer(app)
+const server = http.createServer(app)
 
 const port = process.env.PORT || 5000
-const hostname = '192.168.1.37'
-server.listen(port , ()=>{
+const hostname = '192.168.1.138'
+server.listen(port , hostname, ()=>{
     console.log('Listenning on '+port)
 })
 
