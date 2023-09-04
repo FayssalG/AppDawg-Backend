a720caf3d4831ca1982f5d8952954ee6c3fe0357
FayssalG <fayssaldimawac123@gmail.com>
2023-09-04 17:32:08 +0100

    resetting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const {Server} = require('socket.io')
-const https = require('https')
+const http = require('http')
 
 //const {getDatabase , ref , set  , get , child} = require('firebase/database')
 
@@ -10,36 +10,27 @@ const db = require('./config/firebase')
 
 
 const app = express()
-const server = https.createServer(app)
+const server = http.createServer(app)
 
 const port = process.env.PORT || 5000
-const hostname = '192.168.1.37'
-server.listen(port , ()=>{
+const hostname = '192.168.1.138'
+server.listen(port , hostname, ()=>{
     console.log('Listenning on '+port)
 })
 
-// Set middleware of CORS 
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "*"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-  
-    next();
-  });
 
-const io = new Server(server )
+const io = new Server(server , {
+    cors : {
+        origin : '*',
+        methods : ['POST' , 'GET']
+    }
+})
 
 
 
 let connectedUsers = {}
 
 io.on('connection'  , (socket)=>{
-    console.log('connected to socket !')
     const id = socket.handshake.query.id
     socket.join(id)
     connectedUsers[id] = 'online'
